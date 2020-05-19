3498b18216359484912f90f01b7bc71fb84bb87f
Bart Geurten <bgeurte@gwdg.de>
2020-05-19 09:40:59 +0200

    testing

--- testCharonRemotely.py
+++ testCharonRemotely.py
@@ -5,11 +5,11 @@ reload(charon)
 x.charon.charon('locustHaemo')
 x.OUTPUT_DIR='/media/gwdg-backup/BackUp/Debbie/analysed'
 start = time.time()
-x.runExperimentAnalysis("/media/gwdg-backup/BackUp/Debbie/cellAna_new/2020-05-06_Lm-neuron-TRB#1.zip")
+x.runExperimentAnalysis("/media/gwdg-backup/BackUp/Debbie/cellAna_new/2019-11-25_Hemocytes-60C#1.zip")
 end = time.time()
 print(end - start)
 start = time.time()
-x.runExperimentAnalysis("/media/gwdg-backup/BackUp/Debbie/cellAna_new/Neuron Test #1.zip")
+x.runExperimentAnalysis("/media/gwdg-backup/BackUp/Debbie/cellAna_new/2020-03-12_dHl-hemocytes#4.zip")
 end = time.time()
 print(end - start)
 
