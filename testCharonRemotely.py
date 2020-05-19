9b06f307a7dce940b17be106eb3b88cd4200d5ff
Bart Geurten <bgeurte@gwdg.de>
2020-05-19 09:44:10 +0200

    testing

--- testCharonRemotely.py
+++ testCharonRemotely.py
@@ -2,7 +2,7 @@ from importlib import reload
 import charon, time,  training_utils
 #standard zipped experiment
 reload(charon)
-x.charon.charon('locustHaemo')
+x = charon.charon('locustHaemo')
 x.OUTPUT_DIR='/media/gwdg-backup/BackUp/Debbie/analysed'
 start = time.time()
 x.runExperimentAnalysis("/media/gwdg-backup/BackUp/Debbie/cellAna_new/2019-11-25_Hemocytes-60C#1.zip")
