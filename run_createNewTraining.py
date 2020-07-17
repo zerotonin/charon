fc45a23c7228b057cc375cf1cc250cecc2cd7f58
zerotonin <bgeurte@gwdg.de>
2020-07-17 23:52:22 +0200

    updated

--- run_createNewTraining.py
+++ run_createNewTraining.py
@@ -3,18 +3,15 @@ import charon, time,  training_utils
 
 reload(training_utils)
 
-t = training_utils.trainDataCuration()
-t.WORK_DIR='/media/dataSSD/trainingData/triboliumNeuron/origData'
-t.TEST_DIR='/media/dataSSD/trainingData/triboliumNeuron/test'
-t.TRAIN_DIR='/media/dataSSD/trainingData/triboliumNeuron/train'
-t.sourceImgType ='tif'
+t = training_utils.trainDataCuration('flyBehav','/media/dataSSD/singleMaleTraining',sourceImgType ='png')
 t.chooseCandidateFiles()
 # t.renameLabelsVerbose() or set dictionary
 t.labelChanger = {'male1': 'male 1', 'agression': 'aggression', 'male 2': 'male 2', 'male 1': 'male 1', 'homo courtship': 'homo courtship', 'male2': 'male 2', 'courtship': 'courtship', 'female': 'female'}
+t.makeTrainDirs()
 t.transfer_trainingData()
 
 
 reload(training_utils)
-g = training_utils.runTrainingGenScripts(t,'triboliumNeuron')
+g = training_utils.runTrainingGenScripts(t)
 g.run()
 
