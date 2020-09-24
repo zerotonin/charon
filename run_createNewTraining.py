e0afc602e155695c1abd2f35a7c21d455bedf359
zerotonin <bgeurte@gwdg.de>
2020-09-24 12:53:21 +0200

    test

--- run_createNewTraining.py
+++ run_createNewTraining.py
@@ -3,7 +3,9 @@ import charon, time,  training_utils
 
 reload(training_utils)
 
-t = training_utils.trainDataCuration('flyBehav','/media/dataSSD/singleMaleTraining',sourceImgType ='png')
+t = training_utils.trainDataCuration('mosquitoDetector',
+                                    '/home/bgeurten/ownCloud/DiegoAIdata/03-05-2020_21-59-04/',
+                                    sourceImgType ='png')
 t.chooseCandidateFiles()
 # t.renameLabelsVerbose() or set dictionary
 t.labelChanger = {'male1': 'male 1', 'agression': 'aggression', 'male 2': 'male 2', 'male 1': 'male 1', 'homo courtship': 'homo courtship', 'male2': 'male 2', 'courtship': 'courtship', 'female': 'female'}
