3478b6345f00b59dfa52b977bf175f914638f675
Bart Geurten <bgeurte@gwdg.de>
2021-02-14 12:13:35 +0100

    more comments and plot functions

--- doubleLinkerFit.py
+++ doubleLinkerFit.py
@@ -20,9 +20,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
+def simpleSigmoid(z,a,b):
+    # This is a simple sigmoid function to test if our curve fitting works.
+    #
+    #   exp(-z ⋅ a + b)  
+    # ───────────────────
+    # 1 + exp(-z ⋅ a + b)
+    
+    return np.exp(-z*a+b)/(1 + np.exp(-z*a+b))
+
 
 def channelFunc(D,m0,Cch,Csp):
+    # This function represents the numpy conform notation of the formula 2.12 on page 20 of:
+    # Albert, Jörg T., Björn Nadrowski, and Martin C. Göpfert. "Mechanical signatures of 
+    # transducer gating in the Drosophila ear." Current Biology 17.11 (2007): 1000-1006
+    #
+    #         ⎛ ⎛     ⎛⎛ Cch⎞    2⎞⎞⎞   
+    #         ⎜ ⎜m0 - ⎜⎜────⎟ ⋅ D ⎟⎟⎟   
+    #         ⎜ ⎜     ⎜⎜   2⎟     ⎟⎟⎟   
+    #         ⎜ ⎜     ⎝⎝Csp ⎠     ⎠⎟⎟   
+    #     exp ⎜-⎜──────────────────⎟⎟   
+    #         ⎝ ⎝        kT        ⎠⎠   
+    # y = ─────────────────────────────────
+    #         ⎛    ⎛ ⎛     ⎛⎛ Cch⎞    2⎞⎞⎞⎞
+    #         ⎜    ⎜ ⎜m0 - ⎜⎜────⎟ ⋅ D ⎟⎟⎟⎟
+    #         ⎜    ⎜ ⎜     ⎜⎜   2⎟     ⎟⎟⎟⎟
+    #         ⎜    ⎜ ⎜     ⎝⎝Csp ⎠     ⎠⎟⎟⎟
+    #     1 + ⎜exp ⎜-⎜──────────────────⎟⎟⎟
+    #         ⎝    ⎝ ⎝        kT        ⎠⎠⎠
+    #
+    # D  =  Stimulus amplitude
     # kT =  Boltzman Konstant at Roomtemperatur ~ 4.1 zJ 
+
     kT = 4.1
     return (np.exp(-((m0-((Cch/Csp**2)*D**2))/kT)))/(1+(np.exp(-((m0-((Cch/Csp**2)*D**2))/kT))))
 
@@ -50,22 +79,40 @@ def plotRawData(wt,dl):
     plt.plot(wt[:,0],wt[:,1],'o',mfc="lightgray",mec="k",label='wildtype data')
     plt.plot(dl[:,0],dl[:,1],'o',mfc="dimgray"  ,mec="k",label='double linker data')
 
-def plotFit(data,popt,labelStr,col ='r'):
-    plt.plot(data[:,0], channelFunc(data[:,0], *popt),color=col,label= 'fit for ' +labelStr+ ': m0=%5.3f, Cch=%5.3f, Csp=%5.3f' % tuple(popt))
+def plotFit(data,popt,dataStr,col ='dimgray'):
+    if len(popt) == 3:
+        labelStr = 'fit for ' +dataStr+ ': m0=%5.3f, Cch=%5.3f, Csp=%5.3f' % tuple(popt)
+        plt.plot(data[:,0], channelFunc(data[:,0], *popt),color=col,label= labelStr)
+    else:
+        labelStr = 'fit for ' +dataStr+ ': a=%5.3f,b=%5.3f' % tuple(popt)
+        plt.plot(data[:,0], simpleSigmoid(data[:,0], *popt),color=col,label= labelStr)
+
 
 def finalisePlot():
     plt.xlabel('absolute pressure stimulus, mmHq')
     plt.ylabel('normalised cell response, aU')
-    plt.legend()
+    plt.legend(loc='upper left',fancybox=True, shadow=True)
     plt.show()
 
+
+
+# ██████  ██    ██ ███    ██     ███████ ██ ████████ ███████ 
+# ██   ██ ██    ██ ████   ██     ██      ██    ██    ██      
+# ██████  ██    ██ ██ ██  ██     █████   ██    ██    ███████ 
+# ██   ██ ██    ██ ██  ██ ██     ██      ██    ██         ██ 
+# ██   ██  ██████  ██   ████     ██      ██    ██    ███████ 
+
+
+# import the raw data
 wt,dl = importData()
-
-popt, pcov = curve_fit(channelFunc, wt[:,0], wt[:,1], bounds=(0, [100000., 100000., 100000.]))
-
-
-
-
+dl = np.vstack((np.array([0.,0.]),dl))
+# preform simple fit
+popt, pcov = curve_fit(channelFunc, wt[:,0], wt[:,1],p0=[25,1000,600])#, bounds=(0, [1000000., 1000000., 1000000.]))
+popt2, pcov2 = curve_fit(channelFunc, dl[:,0], dl[:,1],p0=[25,1000,600])#, bounds=(0, [1000000., 1000000., 1000000.]))
+# plot result
+plt.figure()
 plotRawData(wt,dl)
-plotFit(wt,popt,'wt')
+plotFit(wt,popt,'wt','lightgray')
+plotFit(dl,popt2,'dl','gray')
+print(popt)
 finalisePlot()
