3e9cb9e14a03bad6592fad523e863ceda43db8cb
Bart Geurten <bgeurte@gwdg.de>
2021-09-22 10:48:11 +0200

    started a presenter / plotter class

---
+++ charonPresenter.py
@@ -0,0 +1,52 @@
+
+import cv2
+from bounding_box import bounding_box as bb
+import os
+
+class charonPresenter():
+
+    def __init__(self,mediaFile,traFilePD,mode,frameNo=0):
+        self.mediaFile       = mediaFile
+        self.traFilePD       = traFilePD
+        self.mode            = mode
+        self.frameNo         = frameNo
+        self.videoReaderOpen = False
+        self.saveFlag        = False
+        self.saveFormat      = None # 'image' or 'video'          
+        self.savePos         = None
+    
+    def main(self):
+        detections = self.getDetections()
+        image      = self.getImage()
+        self.presentImage()
+        if self.saveFlag:
+            self.saveImage()
+
+
+    def getImage(self):
+        if self.mode == 'video':
+            if self.videoReaderOpen:
+                image = self.readVideoFrame(self.frameNo)
+            else:
+                self.openVideoReader()
+                image = self.readVideoFrame(self.frameNo)
+        elif self.mode == 'image':
+            image = cv2.imread(self.mediaFile, cv2.IMREAD_COLOR)
+        else:
+            raise ValueError(f'charonPresenter:main:media modus {self.mode} not implemented.')
+        
+    def openVideoReader(self):
+        self.cap = cv2.VideoCapture(self.mediaFile)
+        self.videoReaderOpen = True
+        
+    def getDetections(self):
+        pass
+
+    def getDetections(self):
+        pass
+
+    def presentImage(self):
+        pass
+    
+    def saveImage(self):
+        pass
