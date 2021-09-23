3ca339b6fd2d59a161278b3eae519ae52e51fe17
Bart Geurten <bgeurte@gwdg.de>
2021-09-23 13:06:47 +0200

    now with scaling

--- charonMovTraAna.py
+++ charonMovTraAna.py
@@ -63,5 +63,5 @@ movAna.calculateCenter()
 df = movAna.df
 df.to_hdf(detF,key='df')
 reload(charonPresenter)
-cp = charonPresenter.charonPresenter(movF,detF,mode='video',frameNo =30445)
+cp = charonPresenter.charonPresenter(movF,detF,mode='video',frameNo =30445,imageScale=0.5)
 cp.main()
